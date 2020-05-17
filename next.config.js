54a3fa6e4ed11cd34dcccaca39aa6c99f346e24d
isaac yerima <yerimajohnisaac@gmail.com>
2020-05-17 15:37:28 -0700

    latest update

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require('next-pwa');
+module.exports = withPWA({
+  pwa: {
+    dest:'public',
+    disable: false,
+    register: true,
+    scope: '/',
+    sw: 'service-worker.js',
+    //...
+  }
+})
+
